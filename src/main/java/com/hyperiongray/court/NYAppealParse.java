f1f8dfd696b23d09a2b38e7396a6a284dc07e7e5
Mark Kerzner <mark@elephantscale.com>
2015-05-21 22:08:24 -0500

    parsing work cont'd

--- src/main/java/com/hyperiongray/court/NYAppealParse.java
+++ src/main/java/com/hyperiongray/court/NYAppealParse.java
@@ -1,11 +1,16 @@
 package com.hyperiongray.court;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by mark on 5/20/15.
  */
 public class NYAppealParse {
-    String extracts[][] = {
-            {"Casenumber", "regex"},
+    public static String extracts[][] = {
+            {"Casenumber", "\\[.+\\]"},
             {"DocumentLength", "regex"},
             {"Court", "regex"},
             {"County", "regex"},
@@ -29,6 +34,19 @@ public class NYAppealParse {
             buffer.append(extracts[row][0]).append(":").append(extracts[row][1]).append("\n");
         }
         return buffer.toString();
+    }
+    public Map<String, String> extractInfo(String text) {
+        Map <String, String> info = new HashMap<>();
+        for (int e = 0; e < extracts.length; ++e) {
+            String key = extracts[e][0];
+            String regex = extracts[e][1];
+            Matcher m = Pattern.compile(regex).matcher(text);
+            if (m.find()) {
+                String value = m.group();
+                info.put(key, value);
+            }
+        }
+        return info;
     }
 }
 
--- src/test/java/com/hyperiongray/court/NYAppealParseTest.java
+++ src/test/java/com/hyperiongray/court/NYAppealParseTest.java
@@ -1,10 +1,16 @@
 package com.hyperiongray.court;
 
+import com.google.common.io.Files;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -17,6 +23,24 @@ public class NYAppealParseTest {
     @Test
     public void testConnect() throws IOException {
         NYAppealParse instance = new NYAppealParse();
+        System.out.println(instance.toString());
+    }
+    @Test
+    public void testRegex() throws IOException {
+        NYAppealParse instance = new NYAppealParse();
+        File[] files = new File("test-data/ny_appeals").listFiles();
+        Arrays.sort(files);
+        for (File file: files) {
+            System.out.println("File: " + file.getName());
+            String text= FileUtils.readFileToString(file);
+            Map<String, String> answer = instance.extractInfo(text);
+            for (int e = 0; e < NYAppealParse.extracts.length; ++e) {
+                String key = NYAppealParse.extracts[e][0];
+                if (answer.containsKey(key)) {
+                    System.out.println(key + ": " + answer.get(key));
+                }
+            }
+        }
         System.out.println(instance.toString());
     }
 }

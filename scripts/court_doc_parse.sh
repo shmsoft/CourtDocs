3158b9047546b23a35b7139f80cf3d1b71f85db0
Mark Kerzner <mark@elephantscale.com>
2015-08-22 23:24:22 -0500

    doc

--- scripts/court_doc_parse.sh
+++ scripts/court_doc_parse.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-#FILE_NAME=/home/mark/projects/BDE/data/zip-codes/esiid_zipcode.csv
-FILE_NAME=~/jim/esiid_zipcode.csv
-java -cp target/bde_smt-1.0-SNAPSHOT-jar-with-dependencies.jar com.bde.hbase.HBaseOps --type zip --input $FILE_NAME --environment cloudera
+java -cp target/CourtDoc-1.0-SNAPSHOT-jar-with-dependencies.jar \
+com.hyperiongray.court.NYAppealParse \
+-i ../court_docs/downloads/txt/ -o ../court_docs/downloads/parsed
